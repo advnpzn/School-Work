06f1d5483ce957f8ad759eddaf09d3c2a4f20e88
adenosinetp10 <63637385+adenosinetp10@users.noreply.github.com>
2020-11-03 11:47:23 +0530

    Program to reverse integer.

---
+++ geeeez.py
@@ -0,0 +1,11 @@
+#Python program to reverse the given Integer.
+num = int(input("Enter the Number you want to reverse : ")) 
+print("Number : ",num)                                     
+rev = 0                                                     
+while num > 0:                                              
+    rem = num % 10                                          
+    rev = (rev * 10) + rem                                
+    num//=10                                                
+else:
+    print("Reversed Number : ",rev)
+    pass
