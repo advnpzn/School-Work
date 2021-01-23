45e13906d573cf2b68e9a7cc4bf3fa24f7a0f2fb
adenosinetp10 <63637385+adenosinetp10@users.noreply.github.com>
2021-01-23 17:36:43 +0530

    Create Queue_Operations_using_Linked List.c

---
+++ Queue_Operations_using_Linked List.c
@@ -0,0 +1,93 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Node
+{
+    int val;
+    struct Node *next;
+};
+typedef struct Node N;
+
+N *FRONT = NULL;
+N *REAR = NULL;
+
+void enQUEUE()
+{
+    int value;
+    N *newNode = malloc(sizeof(N));
+    printf("\nEnter the Value to Insert : ");
+    scanf("%d", &value);
+    newNode->val = value;
+    newNode->next = NULL;
+    if (FRONT == NULL)
+    {
+        FRONT = newNode;
+        REAR = FRONT;
+    }
+    else
+    {
+        REAR->next = newNode;
+        REAR = newNode;
+    }
+    printf("\nSuccessfull!\n");
+}
+
+void deQUEUE()
+{
+    if (FRONT == NULL)
+    {
+        printf("STACK IS EMPTY!");
+    }
+    else
+    {
+        N *temp = FRONT;
+        printf("Dequeued %d .", temp->val);
+        FRONT = FRONT->next;
+        free(temp);
+        printf("\nSuccessful!\n");
+    }
+}
+
+void display()
+{
+    if (FRONT == NULL)
+    {
+        printf("STACK IS EMPTY!");
+    }
+    else
+    {
+        N *temp = FRONT;
+        while (temp != NULL)
+        {
+            printf("%d--->", temp->val);
+            temp = temp->next;
+        }
+    }
+}
+
+void main()
+{
+    int choose;
+    do
+    {
+        printf("\n-------CHOOSE----------\n");
+        printf("1.enQUEUE\n2.deQUEUE\n3.Display\n4.EXIT\n ");
+        scanf("%d", &choose);
+        switch (choose)
+        {
+        case 1:
+            enQUEUE();
+            break;
+        case 2:
+            deQUEUE();
+            break;
+        case 3:
+            display();
+            break;
+        case 4:
+            exit(0);
+        default:
+            printf("\nChoose 1-4.");
+        }
+    } while (1);
+}
